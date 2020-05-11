08f904088d403f516541939eb06290b398b8f79a
Edward Feng <edward0414@gmail.com>
2020-05-10 21:39:50 -0400

    fix name

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { query1Url, scrapingUrl, query1Fields, query2Url, query2Fields } from './constants';
 import { makeUrl } from './utilities';
 
-class yFinance {
+class YahooFinance {
   getQuote(symbol: string) {
     const url = makeUrl(query1Url, symbol, query1Fields);
 
@@ -11,13 +11,12 @@ class yFinance {
   }
 }
 
-const yFinanceEnginer = new yFinance();
-yFinanceEnginer
-  .getQuote('OXY')
+const YahooFinanceEnginer = new YahooFinance();
+YahooFinanceEnginer.getQuote('OXY')
   .then((resp) => {
     const data = resp.data.quoteResponse;
     console.log('data', data);
   })
   .catch(console.log);
 
-export default yFinance;
+export default YahooFinance;
