8aa203587f1b51a7a07c373e66f9fe17eadad478
Edward Feng <edward0414@gmail.com>
2020-05-10 21:45:53 -0400

    update test

--- src/__tests__/sample.test.ts
+++ src/__tests__/sample.test.ts
@@ -2,9 +2,8 @@ import yFinance from '../index';
 
 const yFinanceEnginer = new yFinance();
 
-test('Test Async', async () => {
-  expect.assertions(1);
-  const data = await yFinanceEnginer.getQuote('OXY');
-  console.log('data', data);
-  expect(data).not.toBe(null);
+test('Test Async', () => {
+  yFinanceEnginer.getQuote('OXY').then((data) => {
+    expect(data).not.toBe(null);
+  });
 });
